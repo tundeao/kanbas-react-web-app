c7724f46054e308f78c8f8e00ce04cad499920a8
tundeao <tunde.arabaowoyele@gmail.com>
2024-04-04 13:10:44 -0400

    fixed json naming issue

--- src/Kanbas/Database/index.tsx
+++ src/Kanbas/Database/index.tsx
@@ -1,8 +1,5 @@
-import courses from "./courses.js";
-import modules from "./modules.js";
-import assignments from "./assignments.js";
-import users from "./users.js";
-import grades from "./grades.js";
-import enrollment from "./enrollments.js";
-// const links = ["Home", "Modules", "Grades", "Assignments"];
-export default { courses, modules, assignments, users, grades, enrollment };
+import courses from "./courses.json";
+import modules from "./modules.json";
+import assignments from "./assignments.json";
+
+export default {courses, modules, assignments}
