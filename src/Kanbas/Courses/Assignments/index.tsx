94f60ed0ec6fd17fc7d63ab5b17703a0f681f8de
tundeao <tunde.arabaowoyele@gmail.com>
2024-04-04 13:15:27 -0400

    fixed import/export issue

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { FaCheckCircle, FaEllipsisV, FaPlusCircle } from "react-icons/fa";
 import { Link, useParams } from "react-router-dom";
-import { assignments } from "../../Database";
+import db from "../../Database";
 function Assignments() {
   const { courseId } = useParams();
-  const assignmentList = assignments.filter(
+  const assignmentList = db.assignments.filter(
     (assignment) => assignment.course === courseId);
   return (
     <>
